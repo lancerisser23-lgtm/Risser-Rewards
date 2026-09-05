4286302aa1cb9a2f10d337baebf4c4ede588b6ae
lancerisser23-lgtm <lancerisser23@gmail.com>
2026-09-04 23:31:28 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='oak-arrow-v60-10-growth-avatar-fix';
+const CACHE='oak-arrow-v60-11-smart-jobs-vacation-guard';
 const CORE=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png',
 './assets/noah-avatar.png','./assets/eli-avatar.png','./assets/shepherd-avatar.png','./assets/juniper-avatar.png',
 './assets/noah-world.png','./assets/eli-world.png','./assets/shepherd-world.png','./assets/juniper-world.png'];
